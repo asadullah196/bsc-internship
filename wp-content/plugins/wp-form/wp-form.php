9d56031ca10f5e025a63360bf7ae11e1f8501605
Galib <md.asadullah196@gmail.com>
2021-03-12 22:23:40 +0600

    Added new form code in wp-form plgugin file

---
+++ wp-content/plugins/wp-form/wp-form.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: WP Form
+Plugin URI: https://www.galibnotes.com/product/wordpress-form/
+Description: WordPress form is a plugin that takes user registration data and store in mySQL data store. After that, you can read data from the view button.
+Author: Asadullah Galib
+Version: 1.0.0
+Author URI: https://galibnotes.com
+Requires at least: 4.5
+Tested up to: 5.7
+License: GPLv2 or later
+Text Domain: wpform
+*/
+
+// Only "Plugin Name" is required and the rest options are optional.
+?>
