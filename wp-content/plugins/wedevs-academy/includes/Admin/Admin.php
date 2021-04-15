b14c19443b09cf01b499dbea081e56e92fd32376
Galib <md.asadullah196@gmail.com>
2021-04-16 00:49:46 +0600

    Plugin menu added with dashicon

--- wp-content/plugins/wedevs-academy/includes/Admin/Admin.php
+++ wp-content/plugins/wedevs-academy/includes/Admin/Admin.php
@@ -1,2 +1,26 @@
 <?php
 
+namespace weDevs\Academy\Admin;
+
+/**
+ * The menu handler class
+ */
+class Menu{
+    function __construct(){
+        add_action( 'admin_menu', [$this, 'admin_menu'] );
+    }
+
+    public function admin_menu(){
+        add_menu_page( 
+            __( 'weDevs Academy', 'wedevs-academy'),
+            __( 'Academy', 'wedevs-academy'),
+            'manage_options',
+            'wedevs-academy',
+            [ $this, 'plugin_page'],
+            'dashicons-admin-tools'
+     );
+    }
+    public function plugin_page(){
+        echo 'Hello Galib';
+    }
+}
