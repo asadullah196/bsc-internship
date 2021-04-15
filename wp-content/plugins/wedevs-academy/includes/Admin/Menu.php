22c4b05c523948ca5db6448c474904b7d1e0e2f1
Galib <md.asadullah196@gmail.com>
2021-04-16 01:28:01 +0600

    New Variable added for re-use

--- wp-content/plugins/wedevs-academy/includes/Admin/Menu.php
+++ wp-content/plugins/wedevs-academy/includes/Admin/Menu.php
@@ -11,14 +11,24 @@ class Menu{
     }
 
     public function admin_menu(){
+        $parent_slug = 'wedevs-academy';
+        $capability = 'manage_options';
+
         add_menu_page( 
             __( 'weDevs Academy', 'wedevs-academy'),
             __( 'Academy', 'wedevs-academy'),
-            'manage_options',
-            'wedevs-academy',
+            $capability,
+            $parent_slug,
             [ $this, 'plugin_page'],
             'dashicons-admin-tools'
-     );
+        );
+        add_submenu_page (
+            $parent_slug,
+            __( 'Address Book', 'wedevs-academy'),
+            __( 'Address Book', 'wedevs-academy'),
+            $capability,
+            
+        );
     }
     public function plugin_page(){
         echo 'Hello Galib';
