259461d09a8622e8dcd44c3816460401fe8c0293
Galib <md.asadullah196@gmail.com>
2021-04-16 16:30:44 +0600

    Internal page design optimize with new class

--- wp-content/plugins/wedevs-academy/includes/Admin/views/address-list.php
+++ wp-content/plugins/wedevs-academy/includes/Admin/views/address-list.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-    <h1><?php _e( 'Address Book P', 'wedevs-academy' ); ?></h1>
+    <h1 class="wp-heading-inline"><?php _e( 'Address Book P', 'wedevs-academy' ); ?></h1>
 
-    <a href="<?php echo admin_url( 'admin.php?page=wedevs-academy&action=new' ) ?>"><?php _e( 'Add New', 'wedevs-academy' ); ?></a>
+    <a href="<?php echo admin_url( 'admin.php?page=wedevs-academy&action=new' ) ?>" class="page-title-action"><?php _e( 'Add New', 'wedevs-academy' ); ?></a>
 </div>
---
+++ wp-content/plugins/wedevs-academy/includes/Admin/views/address-new.php
@@ -0,0 +1,3 @@
+<div class="wrap">
+    <h1><?php _e( 'New address goes here', 'wedevs-academy' ); ?></h1>
+</div>
