08c89c5f624923611b9431a83a064e4a5878389c
Galib <md.asadullah196@gmail.com>
2021-04-16 16:13:05 +0600

    Switch case added with view folder

--- wp-content/plugins/wedevs-academy/includes/Admin/Addressbook.php
+++ wp-content/plugins/wedevs-academy/includes/Admin/Addressbook.php
@@ -8,5 +8,27 @@ namespace weDevs\Academy\Admin;
 class Addressbook {
     public function plugin_page() {
         $action = isset( $_GET['action'] ) ? $_GET['action'] : 'list';
+
+        switch ( $action ){
+            case 'new':
+                $template = __DIR__ . '/view/address-new.php';
+                break;
+
+            case 'edit':
+                $template = __DIR__ . '/view/address-edit.php';
+                break;
+
+            case 'view':
+                $template = __DIR__ . '/view/address-view.php';
+                break;
+
+            default:
+                $template = __DIR__ . '/view/address-list.php';
+                break;
+        }
+
+        if( file_exists( $template ) ) {
+            include $template;
+        }
     }
 }
