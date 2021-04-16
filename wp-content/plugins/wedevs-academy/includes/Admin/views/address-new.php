163c4104965c2ac31e776d7d3fe9d035045efad0
Galib <md.asadullah196@gmail.com>
2021-04-16 16:48:31 +0600

    input form basic added

--- wp-content/plugins/wedevs-academy/includes/Admin/views/address-new.php
+++ wp-content/plugins/wedevs-academy/includes/Admin/views/address-new.php
@@ -1,3 +1,36 @@
 <div class="wrap">
-    <h1><?php _e( 'New address goes here', 'wedevs-academy' ); ?></h1>
+    <h1><?php _e( 'Add New Address', 'wedevs-academy' ); ?></h1>
+
+    <form action="" method="post">
+        <table class="form-table">
+            <tbody>
+                <tr>
+                    <th scope="row">
+                        <lebel for="name"><?php _e( 'Name', 'wedevs-academy' ) ?></label>
+                    </th>
+                    <td>
+                        <input type="text" name="name" id="name" class="regular-text" value="" placeholder="Your Name">
+                    </td>
+                </tr>
+                
+                <tr>
+                    <th scope="row">
+                        <lebel for="address"><?php _e( 'Address', 'wedevs-academy' ) ?></label>
+                    </th>
+                    <td>
+                        <textarea type="text" name="address" id="address" class="regular-text" value="" placeholder="House, Road, Dubai-3310"></textarea>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <th scope="row">
+                        <lebel for="phone"><?php _e( 'Phone', 'wedevs-academy' ) ?></label>
+                    </th>
+                    <td>
+                        <input type="text" name="phone" id="phone" class="regular-text" value="" placeholder="01710 0000 00">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
 </div>
