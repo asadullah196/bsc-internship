780c92f79797ba55bf75b7c56a15188433d46da1
Galib <md.asadullah196@gmail.com>
2021-04-17 01:52:08 +0600

    Installer class added to reduce the main file szie

---
+++ wp-content/plugins/wedevs-academy/includes/Installer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace weDevs\Academy;
+
+/**
+ * Installer class
+ */
+class Installer {
+    public function run() {
+        $this -> add_version();
+        $this -> create_tables();
+    }
+
+    public function add_version() {
+        $installed = get_option( 'wd_academy_installed');
+
+        if (! $installed ){
+            update_option ( 'wd_academy_installed', time() );
+        }
+
+        update_option( 'wd_academy_version', WD_ACADEMY_VERSION );
+    }
+
+    public function create_tables() {
+
+    }
+}
