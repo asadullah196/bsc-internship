973bac5cabbaa2b843b51ea741693f3de2b5e587
Galib <md.asadullah196@gmail.com>
2021-04-16 16:22:59 +0600

    Internal Page Linking done. Uff

---
+++ wp-content/plugins/wedevs-academy/includes/Admin/views/address-list.php
@@ -0,0 +1,5 @@
+<div class="wrap">
+    <h1><?php _e( 'Address Book P', 'wedevs-academy' ); ?></h1>
+
+    <a href="<?php echo admin_url( 'admin.php?page=wedevs-academy&action=new' ) ?>"><?php _e( 'Add New', 'wedevs-academy' ); ?></a>
+</div>
