44f6056a867eda7ebcbc211e255803c3a2f0edcc
Galib <md.asadullah196@gmail.com>
2021-04-17 15:03:14 +0600

    Defult valude added with  variable

--- wp-content/plugins/wedevs-academy/includes/functions.php
+++ wp-content/plugins/wedevs-academy/includes/functions.php
@@ -1,5 +1,21 @@
 <?php
 
-function wd_ac_insert_address() {
-     vb
+function wd_ac_insert_address( $args = [] ) {
+     global $wpdb;
+
+     $defults = [
+         'name' => '',
+         'address' => '',
+         'phone' => '',
+         'created_by' => get_current_user_id(),
+         'created_at' => current_time( 'mysql' ),
+     ];
+
+     $data = wp_parse_args ( $args, $defults ); 
+
+     $wpdb->insert(
+         "{$wpdb->prefix}ac_addresses",
+         $data,
+         $format
+     );
 }
