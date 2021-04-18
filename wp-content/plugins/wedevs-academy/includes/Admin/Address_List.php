dfa8a552a4d8e1ae2083905b530cb378442936c1
Galib <md.asadullah196@gmail.com>
2021-04-19 02:39:30 +0600

    Tutorial finish upto 12 minute and I added upto address list class

--- wp-content/plugins/wedevs-academy/includes/Admin/Address_List.php
+++ wp-content/plugins/wedevs-academy/includes/Admin/Address_List.php
@@ -5,3 +5,13 @@ namespace weDevs\Academy\Admin;
 if ( ! class_exists( 'WP_List_Table' ) ) {
     require_once ABSPATH. 'wp-admin/includes/class-wp-list-table.php';
 }
+
+class Address_List extends \WP_List_Table {
+    function __construct() {
+        parrent::__construct([
+            'singular' => 'contact',
+            'plural' => 'contacts',
+            'ajax' => false
+        ]);
+    }
+}
