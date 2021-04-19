641bdb9d1ebff76961479c6b47bea338d7f50e7f
Galib <md.asadullah196@gmail.com>
2021-04-20 02:33:43 +0600

    functions php file updated and bug fix

--- wp-content/plugins/wedevs-academy/includes/functions.php
+++ wp-content/plugins/wedevs-academy/includes/functions.php
@@ -1,40 +1,40 @@
 <?php
 
 /**
- * Insert a new address 
- * 
- * @param array $args
- * 
- * @return int/WP_Error
+ * Insert a new address
+ *
+ * @param  array  $args
+ *
+ * @return int|WP_Error
  */
 function wd_ac_insert_address( $args = [] ) {
-     global $wpdb;
+    global $wpdb;
 
-     if( empty( $args['name'] ) ) {
-        return new \WP_Error( 'no-name', __('You must provide your name', 'wedevs-academy' ) );
-     }
+    if ( empty( $args['name'] ) ) {
+        return new \WP_Error( 'no-name', __( 'You must provide a name.', 'wedevs-academy' ) );
+    }
 
-     $defults = [
-         'name' => '',
-         'address' => '',
-         'phone' => '',
-         'created_by' => get_current_user_id(),
-         'created_at' => current_time( 'mysql' ),
-     ];
+    $defaults = [
+        'name'       => '',
+        'address'    => '',
+        'phone'      => '',
+        'created_by' => get_current_user_id(),
+        'created_at' => current_time( 'mysql' ),
+    ];
 
-     $data = wp_parse_args( $args, $defults ); 
+    $data = wp_parse_args( $args, $defaults );
 
-     $inserted = $wpdb->insert(
-         $wpdb->prefix . 'ac_addresses',
-         $data,
-         [
-             '%s',
-             '%s',
-             '%s',
-             '%d',
-             '%s'
-         ]
-     );
+    $inserted = $wpdb->insert(
+        $wpdb->prefix . 'ac_addresses',
+        $data,
+        [
+            '%s',
+            '%s',
+            '%s',
+            '%d',
+            '%s'
+        ]
+    );
 
     if ( ! $inserted ) {
         return new \WP_Error( 'failed-to-insert', __( 'Failed to insert data', 'wedevs-academy' ) );
@@ -45,42 +45,42 @@ function wd_ac_insert_address( $args = [] ) {
 
 /**
  * Fetch Addresses
- * 
- * @param array $args
- * 
+ *
+ * @param  array  $args
+ *
  * @return array
  */
 function wd_ac_get_addresses( $args = [] ) {
     global $wpdb;
 
-    $defults = [
-        'number' => 20,
-        'offset' => 0,
+    $defaults = [
+        'number'  => 20,
+        'offset'  => 0,
         'orderby' => 'id',
-        'order' => 'ASC'
+        'order'   => 'ASC'
     ];
 
-    $args = wp_parse_args( $args, $defults );
+    $args = wp_parse_args( $args, $defaults );
 
-    $items = $wpdb->get_results(
-        $wpdb -> prepare(
-            "SELECT * FROM {$wpdb->prefix}ac_adresses
-            ORDER BY %s %s
+    $sql = $wpdb->prepare(
+            "SELECT * FROM {$wpdb->prefix}ac_addresses
+            ORDER BY {$args['orderby']} {$args['order']}
             LIMIT %d, %d",
-            $args['orderby'], $args['order'], $args['offset'], $args['number']
-        )
+            $args['offset'], $args['number']
     );
+
+    $items = $wpdb->get_results( $sql );
 
     return $items;
 }
 
 /**
  * Get the count of total address
- * 
+ *
  * @return int
  */
-function wd_ac_address_count( ) {
+function wd_ac_address_count() {
     global $wpdb;
 
-    return (int) $wpdb->get_var( "SELECT count(id) FROM {$wpdb->prefix}ac_adresses" );
+    return (int) $wpdb->get_var( "SELECT count(id) FROM {$wpdb->prefix}ac_addresses" );
 }
